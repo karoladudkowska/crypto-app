eb6c574a52af766ffd6c944a7289bc3075004004
Karolina Dudkowska <karoladudkowska@wp.pl>
2021-02-19 18:29:53 +0100

    crypto icon

---
+++ src/Crypto.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Crypto.css';
+
+class Crypto extends Component {
+
+    render() {
+
+        return (
+            <div className="Crypto">
+                <ul>
+                    <li>BTC/USD</li>
+                    <li>BTC/GBP</li>
+
+                </ul>
+
+            </div>
+        );
+    }
+}
+
+export default Crypto;
